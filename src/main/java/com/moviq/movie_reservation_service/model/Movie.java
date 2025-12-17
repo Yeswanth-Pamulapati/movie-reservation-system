acadbb512e2e05a02374ebe4fbbe114fd0335ac8
Yeswanth <pamulapatiy@gmail.com>
2025-12-17 12:34:03 +0530

    Commit 4 on Dec 17,2025 to Master

---
+++ src/main/java/com/moviq/movie_reservation_service/model/Movie.java
@@ -0,0 +1,44 @@
+package com.moviq.movie_reservation_service.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private long id;
+    @NotBlank
+    @Size(max = 255)
+    private String title;
+    @Size(max = 1000)
+    private String description;
+    @NotNull
+    @Min(1) // represents 60 mins
+    @Max(600)
+    private int durationMinutes;
+    @NotBlank
+    @Pattern(regexp = "^[A-Za-z]{2}$")//ISO 639‑1 Standard
+    private List<String> languages;
+    @DecimalMin("0.0")
+    @DecimalMax("10.0")
+    private Double rating;
+    @NotNull
+    private LocalDate releaseDate;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+
+}
---
+++ src/main/java/com/moviq/movie_reservation_service/model/Theatre.java
@@ -0,0 +1,28 @@
+package com.moviq.movie_reservation_service.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Theatre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String city;
+    @OneToOne
+    @JsonManagedReference
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+
+
+}
